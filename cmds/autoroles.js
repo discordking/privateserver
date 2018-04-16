8a9ccd5ceb0941bd91a07ac921e4b3b3c5de566e
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-16 18:01:30 +0700

    Update autoroles.js

--- cmds/autoroles.js
+++ cmds/autoroles.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 exports.run = async (bot, message, args) => {
-	if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Maaf kamu tidak bisa menggunakan perintah ini!");
+	if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Sorry you can not use this command!");
 	let autorole = JSON.parse(fs.readFileSync("./autorole.json", "utf8"));
 	if (!args[0]) { // jika tidak ada argument makan autorole akan dimatikan
 		autorole[message.guild.id] = {
@@ -10,7 +10,7 @@ exports.run = async (bot, message, args) => {
 		fs.writeFile("./autorole.json", JSON.stringify(autorole), (err) => {
 			if (err) console.log(err);
 		});
-		message.channel.send(`**${message.author.username}** autorole sudah dimatikan!`);
+		message.channel.send(`**${message.author.username}** autorole is turned off!`);
 	}
 	if (args[0]) { // jika ada argumen maka akan dijadikan autorole
 		let roles = args.join(" ");
@@ -21,7 +21,7 @@ exports.run = async (bot, message, args) => {
 		fs.writeFile("./autorole.json", JSON.stringify(autorole), (err) => {
 			if (err) console.log(err)
 		});
-		message.channel.send(`**${message.author.username}** autorole diset ke **${role.name}**`);
+		message.channel.send(`**${message.author.username}** autorole is set to **${role.name}**`);
 	}
 }
 
