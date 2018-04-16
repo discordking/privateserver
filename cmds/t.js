8d232574921d8f171a4d04b66bdd6cedef992ddd
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-16 22:18:05 +0700

    Update t.js

--- cmds/t.js
+++ cmds/t.js
@@ -4,7 +4,6 @@ exports.run = async (bot, message, args) => {
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return message.channel.send("Can't find user!");
     let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You do not have ``Kick Members`` permissions");
 
        message.guild.member(kUser).ban(kReason);
     
