b13deded45c117b038ae39d6f3e626cff661aec9
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-16 17:47:24 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,9 +32,7 @@ bot.on("message", async message => {
         let args = message.content.slice(prefix.length).trim().split(" ");
         let cmd = args.shift().toLowerCase();
         if (!message.content.startsWith(prefix)) return;
-    
-    if (!message.startsWith(prefix)) return;
-  
+      
     try {
       let commandFile = require(`./cmds/${cmd}.js`);
       commandFile.run(bot, message, args);
