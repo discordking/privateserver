025873db388c8c11307023116f407a6d58252833
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-16 22:13:38 +0700

    Update t.js

--- cmds/t.js
+++ cmds/t.js
@@ -2,10 +2,17 @@ const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Can't find user!");
     let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You do not have ``Kick Members`` permissions");
+
+       message.guild.member(kUser).ban(kReason);
     
-       message.guild.member(kUser);
-    
+  try{      
+
+  } catch (e) {
+      
+  }
 }
 
 module.exports.help = {
