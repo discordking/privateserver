4f06861a03878c6e1ae055096f32e85819a3f3ee
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-17 17:46:33 +0700

    Update welcome.js

--- cmds/welcome.js
+++ cmds/welcome.js
@@ -4,7 +4,7 @@ exports.run = async (bot, message, args) => {
 let ping = Date.now() - message.createdTimestamp
 let embed = new Discord.RichEmbed()
 .setTitle(':bow:  Welcome Command !')
-.addField('how to use welcome commands?','create a channel called `welcome-leave`', true)
+.addField('how to use welcome commands?','create a channel called `member-log`', true)
 .addField('How does it work?','`will automatically welcome new members or members who have been out of the server`', true)
         .setColor("#9A2EFE")
 .setFooter('©Beta | By: Alfian Verter | 2018')
