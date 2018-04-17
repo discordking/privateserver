4813844c1d139a98063345b21f75d9dea77676d7
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-17 17:29:54 +0700

    Update warn.js

--- cmds/warn.js
+++ cmds/warn.js
@@ -1,26 +1,30 @@
 const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {
-  let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!rUser) return message.channel.send("Could't find `user`.");
-  let reason = args.join(" ").slice(22);
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Can't find user!");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have ``MANAGE MESSAGES`` permissions");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be Warned!");
 
-  let warnembed = new Discord.RichEmbed()
-  .setTitle("Warned📫")
-   .setColor("#9A2EFE")
-  .addField("Reported User:", `${rUser}`)
-  .addField("Reported By:", `${message.author}`)
-  .addField("Channel:", message.channel)
-  .addField("Time:", message.createdAt)
-  .addField("Reason:", reason);
+    
+  try{      
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("KICK !!")
+     .setColor("#9A2EFE")
+    .addField("Kicked User", `${kUser}}`)
+    .addField("Kicked By", `<@${message.author.id}>`)
+    .addField("Kicked In", message.channel)
+    .addField("Tiime", message.createdAt)
+    .addField("Reason", kReason);
 
-   rUser.send(warnembed);
-
-  message.delete().catch(O_o=>{});
-
-  return;
+    kUser.send(kickEmbed);
+    message.channel.send(`${kUser} just got Warned !\n**Reason:** ${kReason}`);
+  } catch (e) {
+    message.channel.send(`${kUser} just got Warned !\n**Reason:** ${kReason}`);
+  }
 }
 
-exports.help = {
-  name: "report"
+module.exports.help = {
+  name:"Warn"
 }
