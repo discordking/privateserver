92d085ea09b06d7053d8d593bc8dba39031f6c4b
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-19 21:49:48 +0700

    Update pat.js

--- cmds/pat.js
+++ cmds/pat.js
@@ -6,7 +6,7 @@ let {body} = await superagent
 .get("nekos.life/api/pat")
 var catembed = new Discord.RichEmbed()
 .setTitle('I will always wipe you :*')
-        .setColor("#9A2EFE")
+.setColor("#00FFBF")
 .setImage(body.url)
 message.channel.send(catembed)
 }
