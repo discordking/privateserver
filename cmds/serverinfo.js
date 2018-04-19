4ced254dd2f56a5d4b5ae115f29f73b1e5beba03
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-19 23:31:05 +0700

    Update serverinfo.js

--- cmds/serverinfo.js
+++ cmds/serverinfo.js
@@ -1,23 +1,52 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js'); 
+const botconfig = require('../botconfig.json');
 
-exports.run = async (bot, message, args) => {
-  let sicon = message.guild.iconURL;
-  let patrick = new Discord.RichEmbed()
-  .setAuthor(message.guild.name, message.guild.iconURL)
-.setColor("#00FFBF")
-  .setThumbnail(message.guild.iconURL)
-  .setDescription(`Owner: ${message.guild.owner}`)
-  .addField("Member Count:", message.guild.memberCount, true)
-  .addField("Roles:", message.guild.roles.size, true)
-  .addField("Channels:", message.guild.channels.size, true)
-  .addField("Emojis:", message.guild.emojis.size, true)
-  .addField("Region:", message.guild.region, true)
-  .addField('Created:', message.guild.createdAt.toLocaleString(), true)
-.setFooter('©Beta | By: Alfian Verter | 2018')
+module.exports.run = async (bot, message, args) => {
+  let guild = message.guild;
+  let icon = message.guild.iconURL;
 
-  message.channel.send(patrick);
+  let createdAtRaw = guild.createdAt.toDateString();
+  let createdAt = createdAtRaw.split(" ");
+
+  let bots = message.guild.members.filter(m => m.user.bot).size;
+  let humans = message.guild.members.filter(m => !m.user.bot).size;
+  let channels = message.guild.channels.size;
+  let textChannels = message.guild.channels.filter(m => m.type == "text").size;
+  let voiceChannels = message.guild.channels.filter(i => i.type == "voice").size;
+  let emojis = [];
+  guild.emojis.forEach(emoji => {
+  emojis.push(`\`${emoji.name}\``);
+  });
+  emojis.length === 0 ? emojis = "None" : emojis = emojis.join(", ");
+
+  let roles = [];
+  guild.roles.forEach(role => {
+    roles.push(`\`${role.name}\``);
+  });
+  roles = roles.join(", ");
+
+  let embed = new Discord.RichEmbed()
+  .setTitle(`Server Stats`)
+  .setColor(botconfig.white)
+  .setThumbnail(icon)
+  .addField('Guild Name', guild.name, true)
+  .addField('Guild ID', guild.id, true)
+  .addField('Guild Owner', guild.owner, true)
+  .addField('Created At', `${createdAt[0]} ${createdAt[2]} ${createdAt[1]} ${createdAt[3]}`, true)
+  .addField('Region', guild.region.toUpperCase(), true)
+  .addField('Total Members:', guild.memberCount, true)
+  .addField('Bots:', bots, true)
+  .addField('Users:', humans, true)
+  .addField('Verification Level', guild.verificationLevel, true)
+  .addField('Text Channels', textChannels, true)
+  .addField('Text Channels', voiceChannels, true)
+  .addField('Roles', `${guild.roles.size}`, true)
+  .addField('Emojis', `${guild.emojis.size}`, true)
+
+  return message.channel.send(embed);
 }
 
-exports.help = {
-  name: "serverinfo"
+
+module.exports.help = {
+  name: "serverstats"
 }
