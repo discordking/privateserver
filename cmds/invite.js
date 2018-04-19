c421f62ee07c268ae8dde8f6b2d80d0d96a950b0
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-19 23:15:17 +0700

    Update invite.js

--- cmds/invite.js
+++ cmds/invite.js
@@ -1,17 +1,12 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
 
-exports.run = async (bot, message, args) => {
-let embed = new Discord.RichEmbed()
-.setTitle('want to invite this bot to your server? :robot:')
-.addField('Link','[Click Here](https://discordapp.com/oauth2/authorize?client_id=434304339268337665&scope=bot&permissions=8)', true)
-        .setColor("#9A2EFE")
-.setFooter('©Beta | By: Alfian Verter | 2018')
-message.channel.send({embed})
-
+module.exports.run = async (bot, message, args) => {
+  
+   message.guild.channels.get('434304339268337665').createInvite().then(invite =>  
+    message.channel.send(invite.url));
 }
-    
-exports.help = {
-name: "invite"
+
+module.exports.help = {
+  name: "invite"
 }
-    
