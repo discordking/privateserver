f3ec42cc981dc2fea5a647fa5c99327e200439bd
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-22 03:23:30 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,7 @@ bot.on("ready", async () => {
     console.log(`Logged in as : ${bot.user.tag}`);
     console.log(`${bot.user.username} is ready!`)
     
+bot.setUsername("Verter")
             
     function randomStatus() {
         let status = [`on ${bot.guilds.size} servers.`, `with ${bot.users.size.toLocaleString()} users.`, 'mention @Cosmic', 'type )help']
