5c8ac02f4aecc2cea8909672c26eadb9e85395d0
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-26 05:15:31 -0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
   let bicon = bot.user.displayAvatarURL;
   let patrick = new Discord.RichEmbed()
-.setAuthor('🌳  Hello, i am VerterOS  🌳', bot.user.avatarURL)
-.setDescription('this is a feature I give to you | use this prefix `)`')
+.setAuthor('🌳  Hello, i am Exodus  🌳', bot.user.avatarURL)
+.setDescription('this is a feature I give to you | use this prefix `>`')
 .addField('»Anime Image','`neko` `pat`')
 .addField('»Core','`help` `ping` `stats` `welcome`')
 .addField('»Economy','`balance` `addbalance`')
