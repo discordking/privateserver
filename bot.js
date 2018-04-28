ca099df188653c68936155b7a7e9b1b3e033d938
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-28 15:15:30 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ bot.on("ready", async () => {
     console.log(`Logged in as : ${bot.user.tag}`);
     console.log(`${bot.user.username} is ready!`)
     
-bot.user.setUsername("Exodus")
+bot.user.setUsername("Tendo Game")
             
     function randomStatus() {
         let status = [`>on ${bot.guilds.size} servers<`, `>with ${bot.users.size.toLocaleString()} users<`, '>mention @Exodus<', 'Use >help']
