2bb957744e505d402d5c49bd9292b7f6b9417fbd
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-05-01 15:25:03 +0700

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -4,7 +4,7 @@ exports.run = async (bot, message, args) => {
   async function purge() {
       let insert = args.join(" ");
       if (!message.member.permissions.has("MANAGE_MESSAGES")) {
-          message.channel.send('You do not have ``Manage Messages`` permissions');
+          message.channel.send('You do not have ``Manage Messages`` permissions \n to use the command: `PREFIX`');
           message.delete();
           return;
       }
