c7c43a4d31b61a4ef55ee6eb3195412fdf962749
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-05-14 09:16:57 +0700

    Update invite.js

--- cmds/invite.js
+++ cmds/invite.js
@@ -6,7 +6,7 @@ let embed = new Discord.RichEmbed()
 .setTitle('want to invite this bot to your server? :robot:')
 .addField('Link','[Click Here](https://discordapp.com/oauth2/authorize?client_id=434304339268337665&scope=bot&permissions=8)', true)
 .setColor("RANDOM")
-.setFooter('©Beta | By: Cream Cray | 2018')
+.setFooter('©BOT-OFFICIAL-RAINBOW | By: Alfian Verter | 2018')
 message.channel.send({embed})
 
 }
