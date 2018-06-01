d0bc30e4ee208a6d73769d9d2db21556816aaece
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-02 04:39:31 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -17,7 +17,7 @@ exports.run = async (bot, message, args) => {
 .addField('»Music','`play` `pause` `resume` `volume` `stop` `skip` `np` `queue`')
 .setColor("RANDOM")
   .setThumbnail(bicon)
-.setFooter('©Joker-Squad | By: Alfian Verter | 2018')
+.setFooter('©IndoXploit | By: Phenom | 2018')
 message.channel.send(patrick);
 }
 
