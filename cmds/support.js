6a39297f529419051f3a3d81af23a638ffdc035b
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-03 13:40:57 +0700

    Update support.js

--- cmds/support.js
+++ cmds/support.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 
 exports.run = async (bot, message, args) => {
-message.channel.send('`Jika kamu ikhlas bantu kami dibulan puasa ini :( kami sangat membutuhkan member support..` @everyone @here')
+message.channel.send('`Jika kamu ikhlas bantu kami dibulan puasa ini :( kami sangat membutuhkan member support..`')
 message.channel.send('<https://discord.gg/zxW7hf>')
 message.delete();
 }
