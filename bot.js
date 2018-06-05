9517a90bbac5906c710e337660ef5361e76ffad0
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-05 18:22:49 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ bot.on("ready", async () => {
 bot.user.setUsername("Familly Game Night")
             
     function randomStatus() {
-        let status = [`on ${bot.guilds.size}🌎 servers.`, `with ${bot.users.size.toLocaleString()}🎭 users`, 'mention @IndoXploit', 'Use >help']
+        let status = [`on ${bot.guilds.size}🌎 servers.`, `with ${bot.users.size.toLocaleString()}🎭 users`, 'mention @Family Game Night', 'Use >help']
           let rstatus = Math.floor(Math.random() * status.length);
         bot.user.setActivity(status[rstatus], {type: 'STREAMING'});
     }; setInterval(randomStatus, 20000)
