4d1ca4b10b19e513d2a24ec4b6be02cca6143dde
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-07 06:57:26 +0700

    Add files via upload

---
+++ cmds/invitebot.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+
+exports.run = async (bot, message, args) => {
+let embed = new Discord.RichEmbed()
+.setTitle('want to invite this bot to your server? :robot:')
+.addField('Link','[Click Here](https://discordapp.com/oauth2/authorize?client_id=434304339268337665&scope=bot&permissions=8)', true)
+.setColor("RANDOM")
+.setFooter('©BOT-GAME-ZONE | By: Alfian Verter | 2018')
+message.channel.send({embed})
+
+}
+    
+exports.help = {
+name: "invite"
+}
+    
