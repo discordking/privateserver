fc518450a47ce134863120e05882dccadbf8757f
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-06 23:38:56 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -134,10 +134,10 @@ bot.on("guildMemberAdd", member => {
 	member.addRole(role);
 });
 
-bot.on('message', message => {
+client.on('message', message => {
 
     // Variables
-    let prefix = '>';
+    let prefix = '~';
     let msg = message.content.toUpperCase();
     // Lets also add some new variables
     let cont = message.content.slice(prefix.length).split(" "); // This slices off the prefix, then stores everything after that in an array split by spaces.
@@ -167,7 +167,7 @@ bot.on('message', message => {
             // Now that we have the categories we can start the embed
             const embed = new Discord.RichEmbed()
                 .setDescription(`Available Items`)
-                .setColor('RANDOM')
+                .setColor(0xD4AF37)
 
             for (var i = 0; i < categories.length; i++) { // This runs off of how many categories there are. - MAKE SURE YOU DELETE THAT = IF YOU ADDED IT.
 
@@ -239,8 +239,45 @@ bot.on('message', message => {
     }
 
     // Ping - Let's create a quick command to make sure everything is working!
-    if (message.content.toUpperCase() === `${prefix}TOLOL`) {
+    if (message.content.toUpperCase() === `${prefix}PING`) {
         message.channel.send('Pong!');
+    }
+
+    // Add / Remove Money For Admins
+    if (msg.startsWith(`${prefix}ADDMONEY`)) {
+
+        // Check if they have the modRole
+        if (!message.member.roles.find("name", modRole)) { // Run if they dont have role...
+            message.channel.send('**You need the role `' + modRole + '` to use this command...**');
+            return;
+        }
+
+        // Check if they defined an amount
+        if (!args[0]) {
+            message.channel.send(`**You need to define an amount. Usage: ${prefix}BALSET <amount> <user>**`);
+            return;
+        }
+
+        // We should also make sure that args[0] is a number
+        if (isNaN(args[0])) {
+            message.channel.send(`**The amount has to be a number. Usage: ${prefix}BALSET <amount> <user>**`);
+            return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
+        }
+
+        // Check if they defined a user
+        let defineduser = '';
+        if (!args[1]) { // If they didn't define anyone, set it to their own.
+            defineduser = message.author.id;
+        } else { // Run this if they did define someone...
+            let firstMentioned = message.mentions.users.first();
+            defineduser = firstMentioned.id;
+        }
+
+        // Finally, run this.. REMEMBER IF you are doing the guild-unique method, make sure you add the guild ID to the end,
+        economy.updateBalance(defineduser + message.guild.id, parseInt(args[0])).then((i) => { // AND MAKE SURE YOU ALWAYS PARSE THE NUMBER YOU ARE ADDING AS AN INTEGER
+            message.channel.send(`**User defined had ${args[0]} added/subtraction from their account.**`)
+        });
+
     }
 
     // Balance & Money
