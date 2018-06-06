47ab95b8260e62ea71db257228429490ee9ce4b3
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-06 17:01:53 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -243,34 +243,6 @@ bot.on('message', message => {
         message.channel.send('Pong!');
     }
 
-        // Check if they defined an amount
-        if (!args[0]) {
-            message.channel.send(`**You need to define an amount. Usage: ${prefix}BALSET <amount> <user>**`);
-            return;
-        }
-
-        // We should also make sure that args[0] is a number
-        if (isNaN(args[0])) {
-            message.channel.send(`**The amount has to be a number. Usage: ${prefix}BALSET <amount> <user>**`);
-            return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
-        }
-
-        // Check if they defined a user
-        let defineduser = '';
-        if (!args[1]) { // If they didn't define anyone, set it to their own.
-            defineduser = message.author.id;
-        } else { // Run this if they did define someone...
-            let firstMentioned = message.mentions.users.first();
-            defineduser = firstMentioned.id;
-        }
-
-        // Finally, run this.. REMEMBER IF you are doing the guild-unique method, make sure you add the guild ID to the end,
-        economy.updateBalance(defineduser + message.guild.id, parseInt(args[0])).then((i) => { // AND MAKE SURE YOU ALWAYS PARSE THE NUMBER YOU ARE ADDING AS AN INTEGER
-            message.channel.send(`**User defined had ${args[0]} added/subtraction from their account.**`)
-        });
-
-    }
-
     // Balance & Money
     if (msg === `${prefix}BALANCE` || msg === `${prefix}MONEY`) { // This will run if the message is either ~BALANCE or ~MONEY
 
