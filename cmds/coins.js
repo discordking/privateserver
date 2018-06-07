9a869c0ab33cbb193a1bfbb589d420a1e7afefd9
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-08 03:03:23 +0700

    Add files via upload

---
+++ cmds/coins.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+let coins = require("../coins.json");
+
+module.exports.run = async (bot, message, args) => {
+  //!coins
+  if(!coins[message.author.id]){
+    coins[message.author.id] = {
+      coins: 0
+    };
+  }
+
+  let uCoins = coins[message.author.id].coins;
+
+
+  let coinEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+   .setDescription("Coin Kamu Sekarang, dapatkan coin dengan Online Setiap harinya !!")
+  .setColor("#00FF00")
+  .addField("💸", uCoins)
+  .setFooter('©BETA | By: VERTER | 2018');
+  message.channel.send(coinEmbed).then(msg => {msg.delete(10000)});
+
+}
+
+module.exports.help = {
+  name: "coins"
+}
