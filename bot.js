0a29d948f541784dab41264013559635d7967353
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-07 16:09:26 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,7 @@ const figlet = require('figlet');
 const Command = require("klasa");
 const prefix = require("./config.json");
 let coins = require("./coins.json");
+let xp = require("./xp.json");
 
 
 bot.on("ready", async () => {
@@ -159,6 +160,33 @@ bot.on("message", async message => {
 
   message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
   }
-});
+
+  let xpAdd = Math.floor(Math.random() * 7) + 8;
+  console.log(xpAdd);
+
+  if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+    };
+  }
+
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 300;
+  xp[message.author.id].xp =  curxp + xpAdd;
+  if(nxtLvl <= xp[message.author.id].xp){
+    xp[message.author.id].level = curlvl + 1;
+    let lvlup = new Discord.RichEmbed()
+    .setTitle("Level Up!")
+    .setColor(purple)
+    .addField("New Level", curlvl + 1);
+
+    message.channel.send(lvlup).then(msg => {msg.delete(5000)});
+  }
+  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+    if(err) console.log(err)
+  });
 
 bot.login(process.env.BOT_TOKEN);
