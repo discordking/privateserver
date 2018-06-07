0349e2c27ab69108a273c953ce609873625877d2
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-07 13:53:27 +0700

    Update welcome.js

--- cmds/welcome.js
+++ cmds/welcome.js
@@ -4,10 +4,10 @@ exports.run = async (bot, message, args) => {
 let ping = Date.now() - message.createdTimestamp
 let embed = new Discord.RichEmbed()
 .setTitle(':bow:  Welcome Command !')
-.addField('how to use welcome commands?','create a channel called `member-log`', true)
+.addField('how to use welcome commands?','create a channel called `welcome-goodbye`', true)
 .addField('How does it work?','`will automatically welcome new members or members who have been out of the server`', true)
 .setColor("RANDOM")
-.setFooter('©BOT-GAME-ZONE | By: Alfian Verter | 2018')
+.setFooter('©BETA | By: VERTER | 2018')
 message.channel.send({embed})
 }
 
