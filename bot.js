66a4c557f87380560f51549292062a62b2df1a98
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-07 14:12:54 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,8 @@ const figlet = require('figlet');
 const Command = require("klasa");
 const prefix = require("./config.json");
 const modRole = 'Administrator';
+let coins = require("./coins.json");
+
 
 const items = JSON.parse(fs.readFileSync('items.json', 'utf8'));
 
@@ -134,5 +136,32 @@ bot.on("guildMemberAdd", member => {
 	member.addRole(role);
 });
 
+bot.on("message", async message => {
+
+  if(!coins[message.author.id]){
+    coins[message.author.id] = {
+      coins: 0
+    };
+  }
+
+  let coinAmt = Math.floor(Math.random() * 15) + 1;
+  let baseAmt = Math.floor(Math.random() * 15) + 1;
+  console.log(`${coinAmt} ; ${baseAmt}`);
+
+  if(coinAmt === baseAmt){
+    coins[message.author.id] = {
+      coins: coins[message.author.id].coins + coinAmt
+    };
+  fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+    if (err) console.log(err)
+  });
+  let coinEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#0000FF")
+  .addField("💸", `${coinAmt} coins added!`);
+
+  message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+  }
+});
 
 bot.login(process.env.BOT_TOKEN);
