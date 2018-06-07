681b42e40ceac5c37a6128d57c0ea169838c707d
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-07 14:23:42 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -158,7 +158,7 @@ bot.on("message", async message => {
   let coinEmbed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("#0000FF")
-  .addField("💸", `${coinAmt} coins added!`);
+  .addField("💸", `BOM Coin kamu bertambah !! ${coinAmt} `);
 
   message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
   }
