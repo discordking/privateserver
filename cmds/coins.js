f5c892ea16991c9d9a5528fd192aa8dd84f48289
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-07 15:49:55 +0700

    Update coins.js

--- cmds/coins.js
+++ cmds/coins.js
@@ -16,8 +16,8 @@ module.exports.run = async (bot, message, args) => {
   .setAuthor(message.author.username)
    .setDescription("Coin Kamu Sekarang, dapatkan coin dengan Online Setiap harinya !!")
   .setColor("#00FF00")
-  .addField(`:moneybag: uCoins`)
-.setFooter('©BETA | By: VERTER | 2018');
+  .addField("💸", uCoins)
+  .setFooter('©BETA | By: VERTER | 2018');
   message.channel.send(coinEmbed).then(msg => {msg.delete(10000)});
 
 }
