0cf647ba2897dd9bad4340ea0e57c521bb9bf8ff
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-07 17:39:21 +0700

    Update level.js

--- cmds/level.js
+++ cmds/level.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const botconfig = require("../config");
-let purple = botconfig.purple;
 let xp = require("../xp.json");
 
 module.exports.run = async (bot, message, args) => {
@@ -18,7 +17,7 @@ module.exports.run = async (bot, message, args) => {
 
   let lvlEmbed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
-  .setColor(purple)
+  .setColor('RANDOM')
   .addField("Level", curlvl, true)
   .addField("XP", curxp, true)
   .setFooter(`${difference} XP til level up`, message.author.displayAvatarURL);
