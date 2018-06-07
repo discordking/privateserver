75821a40930084115e9e189af8f50e7ebdfcaab0
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-07 14:48:52 +0700

    Add files via upload

---
+++ cmds/balance.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const economy = require('discord-eco');
+
+exports.run = async (bot, message, args) => {
+
+          economy.fetchBalance(message.author.id + message.guild.id).then((i) => {
+            const embed = new Discord.RichEmbed()
+                .setDescription(`**${message.guild.name} Bank**`)
+.setColor("RANDOM")
+            .addField('Account Holder',message.author.username, true) 
+                .addField('Account Balance',i.money, true)
+
+
+            message.channel.send({embed})
+                    
+          })
+          
+}
+
+exports.help = {
+  name: "say"
+}
