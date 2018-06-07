6f4f652e827833768ed835be7794036738674252
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-08 03:00:58 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,4 +77,70 @@ bot.on("message", async message => {
 
 });
 
+bot.on("message", async autoresponder => {
+    if(autoresponder.author.bot) return;
+    if(autoresponder.channel.type === "dm") return;
+      
+        let msg = autoresponder.content.toLowerCase();
+        let sender = autoresponder.author;
+        if (autoresponder.content.startsWith(prefix)) return;
+    
+    if (autoresponder.content === `<@!${bot.user.id}>`) {
+    autoresponder.react('ðŸ¥');
+    return autoresponder.channel.send(`Hi ${sender},` + ' use this command ``>help`` ')
+    }
+    
+    if (autoresponder.content === `<@${bot.user.id}>`) {
+    autoresponder.react('ðŸ¥');
+    return autoresponder.channel.send(`Hi ${sender},` + ' use this command ``>help`` ')
+    }
+        
+});
+
+bot.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome-goodbye');
+    let memberavatar = member.user.displayAvatarURL;
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+.setColor("RANDOM")
+	.setThumbnail(memberavatar)
+        .addField(':bust_in_silhouette: | name : ', `${member}`)
+        .addField(':microphone2: | Welcome!', `Welcome to the server, ${member}`)
+        .addField(':family_mwgb: | Your are the member', `${member.guild.memberCount}`)
+        .addField("Name", `<@` + `${member.id}` + `>`, true)
+        .addField('Server', `${member.guild.name}`, true )
+        .setFooter(`${member.guild.name}`)
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
+bot.on('guildMemberRemove', member => {
+    let channel = member.guild.channels.find('name', 'welcome-goodbye');
+    let memberavatar = member.user.displayAvatarURL;
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+.setColor("RANDOM")
+	.setThumbnail(memberavatar)
+        .addField(':bust_in_silhouette: | Name:', `${member}`)
+        .addField('Has Let the Server', 'Good Bye !')
+        .addField(':family_mwgb: | The server now as', `${member.guild.memberCount}` + " members")
+        .setFooter(`${member.guild.name}`)
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
+bot.on("guildMemberAdd", member => {
+	let autorole = JSON.parse(fs.readFileSync("./autorole.json", "utf8"));
+	if (!autorole[member.guild.id]) { // jika tidak ada autorole yang di set, agar tidak error saat ada yang join
+		autorole[member.guild.id] = {
+			autorole: config.autorole
+		};
+	}
+	var role = autorole[member.guild.id].role;
+	if (!role) return; // jika autorole 0 maka akan dihentikan dan tidak menyebabkan error
+	member.addRole(role);
+});
+
 bot.login(process.env.BOT_TOKEN);
