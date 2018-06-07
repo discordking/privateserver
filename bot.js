ae6dbfb6aac70af9bd37cb6f983ac0c9a3762504
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-07 16:19:22 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -154,7 +154,6 @@ bot.on("message", async message => {
 
   let coinAmt = Math.floor(Math.random() * 15) + 1;
   let baseAmt = Math.floor(Math.random() * 15) + 1;
-  console.log(`${coinAmt} ; ${baseAmt}`);
 
   if(coinAmt === baseAmt){
     coins[message.author.id] = {
