dcf4578b9c18eb25c2b8622d4fb7bbbf7d8a5d24
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-08 02:51:56 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,148 +14,67 @@ let coins = require("./coins.json");
 let xp = require("./xp.json");
 let cooldown = new Set();
 let cdseconds = 5;
+const botconfig = require("./config.json");
+bot.commands = new Discord.Collection();
 
+fs.readdir("./cmds/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
 
 bot.on("ready", async () => {
-    console.log(`Logged in as : ${bot.user.tag}`);
-    console.log(`${bot.user.username} is ready!`)
-    
-bot.user.setUsername("VERTER")
-            
-    function randomStatus() {
-        let status = [`on ${bot.guilds.size}servers.`, `with ${bot.users.size.toLocaleString()} users`, 'mention @VERTER', 'Use >help']
-          let rstatus = Math.floor(Math.random() * status.length);
-        bot.user.setActivity(status[rstatus], {type: 'STREAMING'});
-    }; setInterval(randomStatus, 20000)
-  });
-  
+
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
+
+});
+
+
 bot.on("message", async message => {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
+
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+      prefixes: botconfig.prefix
+    };
+  }
 	
-	let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-	
-	if(!prefixes[message.guild.id]){
-		prefixes[message.guild.id] = {
-			prefixes: config.prefix
-			
-		};
-	}
-	let prefix = prefixes[message.guild.id].prefixes;
-    
-    let msg = message.content.toLowerCase();
-    let args = message.content.slice(prefix.length).trim().split(" ");
-    let cmd = args.shift().toLowerCase();
-
-   if (!msg.startsWith(prefix)) return;
-      
-    try {
-      let commandFile = require(`./cmds/${cmd}.js`);
-      commandFile.run(bot, message, args);
-    } catch (e) {
-      console.log(e.message)
-    } finally {
-      console.log(`${message.author.tag} menggunakan perintah ${prefix}${cmd}`);
-    }
-  });
-
-bot.on("message", async autoresponder => {
-    if(autoresponder.author.bot) return;
-    if(autoresponder.channel.type === "dm") return;
-      
-        let msg = autoresponder.content.toLowerCase();
-        let sender = autoresponder.author;
-        if (autoresponder.content.startsWith(prefix)) return;
-    
-    if (autoresponder.content === `<@!${bot.user.id}>`) {
-    autoresponder.react('ðŸ¥');
-    return autoresponder.channel.send(`Hi ${sender},` + ' use this command ``>help`` ')
-    }
-    
-    if (autoresponder.content === `<@${bot.user.id}>`) {
-    autoresponder.react('ðŸ¥');
-    return autoresponder.channel.send(`Hi ${sender},` + ' use this command ``>help`` ')
-    }
-        
-});
+  let prefix = prefixes[message.guild.id].prefixes;
+  if(!message.content.startsWith(prefix)) return;
+  if(cooldown.has(message.author.id)){
+    message.delete();
+    return message.reply("You have to wait 5 seconds between commands.")
+  }
+  if(!message.member.hasPermission("ADMINISTRATOR")){
+    cooldown.add(message.author.id);
+  }
 
 
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
 
-bot.on("message", async autoresponder => {
-    if(autoresponder.author.bot) return;
-    if(autoresponder.channel.type === "dm") return;
-      
-        let msg = autoresponder.content.toLowerCase();
-        let sender = autoresponder.author;
-        if (autoresponder.content.startsWith(prefix)) return;
-    
-    if (autoresponder.content === `<@!${bot.user.id}>`) {
-    autoresponder.react('ðŸ¥');
-    return autoresponder.channel.send(`Hi ${sender},` + ' use this command ``>help`` ')
-    }
-    
-    if (autoresponder.content === `<@${bot.user.id}>`) {
-    autoresponder.react('ðŸ¥');
-    return autoresponder.channel.send(`Hi ${sender},` + ' use this command ``>help`` ')
-    }
-        
-});
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
 
-bot.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'welcome-goodbye');
-    let memberavatar = member.user.displayAvatarURL;
-        if (!channel) return;
-        let embed = new Discord.RichEmbed()
-.setColor("RANDOM")
-	.setThumbnail(memberavatar)
-        .addField(':bust_in_silhouette: | name : ', `${member}`)
-        .addField(':microphone2: | Welcome!', `Welcome to the server, ${member}`)
-        .addField(':family_mwgb: | Your are the member', `${member.guild.memberCount}`)
-        .addField("Name", `<@` + `${member.id}` + `>`, true)
-        .addField('Server', `${member.guild.name}`, true )
-        .setFooter(`${member.guild.name}`)
-        .setTimestamp()
+  setTimeout(() => {
+    cooldown.delete(message.author.id)
+  }, cdseconds * 1000)
 
-        channel.sendEmbed(embed);
-});
-
-bot.on('guildMemberAdd', member => {
-
-    console.log(`${member}`, "has joined" + `${member.guild.name}`)
-
-});
-
-bot.on('guildMemberRemove', member => {
-    let channel = member.guild.channels.find('name', 'welcome-goodbye');
-    let memberavatar = member.user.displayAvatarURL;
-        if (!channel) return;
-        let embed = new Discord.RichEmbed()
-.setColor("RANDOM")
-	.setThumbnail(memberavatar)
-        .addField(':bust_in_silhouette: | Name:', `${member}`)
-        .addField('Has Let the Server', 'Good Bye !')
-        .addField(':family_mwgb: | The server now as', `${member.guild.memberCount}` + " members")
-        .setFooter(`${member.guild.name}`)
-        .setTimestamp()
-
-        channel.sendEmbed(embed);
-});
-
-bot.on('guildMemberRemove', member => {
-    console.log(`${member}` + "has left" + `${member.guild.name}` + "Sending leave message now")
-    console.log("Leave Message Sent")
-});
-
-bot.on("guildMemberAdd", member => {
-	let autorole = JSON.parse(fs.readFileSync("./autorole.json", "utf8"));
-	if (!autorole[member.guild.id]) { // jika tidak ada autorole yang di set, agar tidak error saat ada yang join
-		autorole[member.guild.id] = {
-			autorole: config.autorole
-		};
-	}
-	var role = autorole[member.guild.id].role;
-	if (!role) return; // jika autorole 0 maka akan dihentikan dan tidak menyebabkan error
-	member.addRole(role);
 });
 
 bot.login(process.env.BOT_TOKEN);
