734592f0a547e28ee16e34671c76b25c3c48c0e7
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-07 20:19:01 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ bot.on("ready", async () => {
 bot.user.setUsername("VERTER")
             
     function randomStatus() {
-        let status = [`on ${bot.guilds.size}ðŸŒŽ servers.`, `with ${bot.users.size.toLocaleString()}ðŸŽ­ users`, 'mention @VERTER', 'Use >help']
+        let status = [`on ${bot.guilds.size}servers.`, `with ${bot.users.size.toLocaleString()} users`, 'mention @VERTER', 'Use >help']
           let rstatus = Math.floor(Math.random() * status.length);
         bot.user.setActivity(status[rstatus], {type: 'STREAMING'});
     }; setInterval(randomStatus, 20000)
