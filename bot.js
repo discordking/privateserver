b95e810407aba0102757977ce67bca3040e4fe32
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-08 16:52:26 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ fs.readdir("./cmds/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
+  bot.user.setActivity("tutorials on TSC", {type: "STREAMING"});
 
 });
 
